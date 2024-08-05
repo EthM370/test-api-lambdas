a498d2df9c0030256290f2cc039e87faeab910cd
Dev Singh <dsingh14@illinois.edu>
2024-08-05 13:11:00 -0400

    add logging; use esm modules w/ top level await for faster lambda start

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,8 +1,45 @@
+import { randomUUID } from "crypto";
 import fastify from "fastify";
 
+const now = () => Date.now();
+declare module 'fastify' {
+  interface FastifyRequest {
+    startTime: number;
+  }
+};
+
 function init() {
-  const app = fastify();
-  app.get("/api/v1/healthz", (request, reply) => reply.send({ message: "UP" }));
+  const app = fastify({
+    logger: true,
+    disableRequestLogging: true,
+    genReqId: (request) => {
+      const header = request.headers['x-apigateway-event'];
+      if (!header) {
+        return randomUUID().toString();
+      }
+      const typeCheckedHeader = Array.isArray(header) ? header[0] : header;
+      const event = JSON.parse(decodeURIComponent(typeCheckedHeader))
+      return event["requestContext"]['requestId'];
+    }
+  });
+  app.addHook("onRequest", (req, _, done) => {
+    req.startTime = now();
+    req.log.info({ url: req.raw.url }, "received request");
+    done();
+  });
+
+  app.addHook("onResponse", (req, reply, done) => {
+    req.log.info(
+      {
+        url: req.raw.url,
+        statusCode: reply.raw.statusCode,
+        durationMs: now() - req.startTime,
+      },
+      "request completed"
+    );
+    done();
+  });
+  app.get("/api/v1/healthz", (_, reply) => reply.send({ message: "UP" }));
   return app;
 }
 
--- node/src/lambda.ts
+++ node/src/lambda.ts
@@ -3,5 +3,7 @@
 import awsLambdaFastify from "@fastify/aws-lambda";
 import init from ".";
 
-const handler = awsLambdaFastify(init());
+const app = init();
+const handler = awsLambdaFastify(app, {decorateRequest: false, serializeLambdaArguments: true});
+await app.ready() // needs to be placed after awsLambdaFastify call because of the decoration: https://github.com/fastify/aws-lambda-fastify/blob/master/index.js#L9
 export { handler };
